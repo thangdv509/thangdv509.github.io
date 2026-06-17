dd96515329f9182276b57014f04457dd61cba0fb
notta <vietthang.doan@secomus.com>
2026-06-17 10:08:01 +0700

    add: idaho activity

--- src/data/activities.js
+++ src/data/activities.js
@@ -9,9 +9,19 @@ import CommunityDay3 from '../assets/activities/AWSCommunityDay/CommunityDay3.jp
 import CommunityDay4 from '../assets/activities/AWSCommunityDay/CommunityDay4.jpg';
 import CommunityDay5 from '../assets/activities/AWSCommunityDay/CommunityDay5.jpg';
 
+import FairnessInLLMsIdaho1 from '../assets/activities/FairnessInLLMsIdaho/FairnessInLLMsIdaho.jpeg';
+
 const activities = [
   {
     id: 1,
+    images: [FairnessInLLMsIdaho1],
+    event: "Oral Presentation at CIKM 2024 about Fairness in Large Language Models.",
+    organization: "Boise, Idaho, USA",
+    date: "Oct 2024",
+    link: "",
+  },
+  {
+    id: 2,
     images: [TechMaker1, TechMaker2, TechMaker3, TechMaker4],
     event: "From AI-First to AI Adoption",
     organization: "Vietnam Techmakers",
@@ -19,12 +29,12 @@ const activities = [
     link: "https://www.facebook.com/share/v/1KBqeqq7Ej/",
   },
   {
-    id: 2,
+    id: 3,
     images: [CommunityDay1, CommunityDay2, CommunityDay3, CommunityDay4, CommunityDay5],
     event: "AWS First Cloud AI Journey Community Day",
     organization: "AWS First Cloud AI Journey",
-    date: "30 May 2026",
-    link: "",
+    date: "21 May 2026",
+    link: "https://www.facebook.com/share/v/1KBqeqq7Ej/",
   },
 ];
 
