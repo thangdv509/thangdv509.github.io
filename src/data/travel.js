f021c918db092261da8b13acdb04beea5579483d
notta <vietthang.doan@secomus.com>
2026-06-18 14:15:53 +0700

    add 2 more places

--- src/data/travel.js
+++ src/data/travel.js
@@ -1,14 +1,56 @@
+import YConcert20251 from "../assets/travel/Vietnam/Hanoi/YConcert/YConcert20251.jpg";
+import YConcert20252 from "../assets/travel/Vietnam/Hanoi/YConcert/YConcert20252.jpg";
+import YConcert20253 from "../assets/travel/Vietnam/Hanoi/YConcert/YConcert20253.jpg";
+import YConcert20254 from "../assets/travel/Vietnam/Hanoi/YConcert/YConcert20254.jpg";
+import YConcert20255 from "../assets/travel/Vietnam/Hanoi/YConcert/YConcert20255.jpg";
+
+import BankingGrad1 from "../assets/travel/Vietnam/Hanoi/BankingAcademyGraduation/BankingAcademyGraduation1.JPEG";
+import BankingGrad2 from "../assets/travel/Vietnam/Hanoi/BankingAcademyGraduation/BankingAcademyGraduation2.JPEG";
+import BankingGrad3 from "../assets/travel/Vietnam/Hanoi/BankingAcademyGraduation/BankingAcademyGraduation3.JPEG";
+
 import MocChau1 from '../assets/travel/Vietnam/MocChau/MocChau1.jpg';
 import MocChau2 from '../assets/travel/Vietnam/MocChau/MocChau2.jpg';
 import MocChau3 from '../assets/travel/Vietnam/MocChau/MocChau3.jpg';
 import MocChau4 from '../assets/travel/Vietnam/MocChau/MocChau4.jpg';
 import MocChau5 from '../assets/travel/Vietnam/MocChau/MocChau5.jpg';
 
-import YConcert20251 from "../assets/travel/Vietnam/Hanoi/YConcert/YConcert20251.jpg";
-import YConcert20252 from "../assets/travel/Vietnam/Hanoi/YConcert/YConcert20252.jpg";
-import YConcert20253 from "../assets/travel/Vietnam/Hanoi/YConcert/YConcert20253.jpg";
-import YConcert20254 from "../assets/travel/Vietnam/Hanoi/YConcert/YConcert20254.jpg";
-import YConcert20255 from "../assets/travel/Vietnam/Hanoi/YConcert/YConcert20255.jpg";
+import HoaLu1 from "../assets/travel/Vietnam/NinhBinh/HoaLu/HoaLu1.JPEG";
+import HoaLu2 from "../assets/travel/Vietnam/NinhBinh/HoaLu/HoaLu2.JPEG";
+import HoaLu3 from "../assets/travel/Vietnam/NinhBinh/HoaLu/HoaLu3.JPEG";
+import HoaLu4 from "../assets/travel/Vietnam/NinhBinh/HoaLu/HoaLu4.JPEG";
+import HoaLu5 from "../assets/travel/Vietnam/NinhBinh/HoaLu/HoaLu5.JPEG";
+import HoaLu6 from "../assets/travel/Vietnam/NinhBinh/HoaLu/HoaLu6.JPEG";
+import HoaLu7 from "../assets/travel/Vietnam/NinhBinh/HoaLu/HoaLu7.JPEG";
+import HoaLu8 from "../assets/travel/Vietnam/NinhBinh/HoaLu/HoaLu8.JPEG";
+
+import VinhHy1  from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy1.JPEG";
+import VinhHy2  from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy2.JPEG";
+import VinhHy3  from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy3.JPEG";
+import VinhHy4  from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy4.JPEG";
+import VinhHy5  from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy5.JPEG";
+import VinhHy6  from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy6.JPEG";
+import VinhHy7  from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy7.JPEG";
+import VinhHy8  from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy8.JPEG";
+import VinhHy9  from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy9.JPEG";
+import VinhHy10 from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy10.JPEG";
+import VinhHy11 from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy11.JPEG";
+import VinhHy12 from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy12.JPEG";
+import VinhHy13 from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy13.JPEG";
+import VinhHy14 from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy14.JPEG";
+import VinhHy15 from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy15.JPEG";
+import VinhHy16 from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy16.JPEG";
+import VinhHy17 from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy17.JPEG";
+import VinhHy18 from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy18.JPEG";
+import VinhHy19 from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy19.JPEG";
+import VinhHy20 from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy20.JPEG";
+import VinhHy21 from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy21.JPEG";
+import VinhHy22 from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy22.JPEG";
+import VinhHy23 from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy23.JPEG";
+import VinhHy24 from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy24.JPEG";
+import VinhHy25 from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy25.JPEG";
+import VinhHy26 from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy26.JPEG";
+import VinhHy28 from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy28.JPEG";
+import VinhHy29 from "../assets/travel/Vietnam/NinhThuan/VinhHy/VinhHy29.JPEG";
 
 const travelData = [
   {
@@ -27,6 +69,12 @@ const travelData = [
         date: "22 Dec 2025",
         images: [YConcert20251, YConcert20252, YConcert20253, YConcert20254, YConcert20255],
       },
+      {
+        id: 2,
+        name: "Banking Academy Graduation",
+        date: "25 Oct 2025",
+        images: [BankingGrad1, BankingGrad2, BankingGrad3],
+      },
     ],
   },
   {
@@ -36,6 +84,7 @@ const travelData = [
     flag: "🇻🇳",
     lat: 20.8360,
     lng: 104.6800,
+    images: [],
     date: "November 2025",
     events: [
       {
@@ -43,6 +92,49 @@ const travelData = [
         name: "A memorable trip to pine forest, pet farm, little forest, a garden, pink grass hill with my beloved friend",
         date: "Nov 2025",
         images: [MocChau1, MocChau2, MocChau3, MocChau4, MocChau5],
+      },
+    ],
+  },
+  {
+    id: 3,
+    city: "Ninh Binh",
+    country: "Vietnam",
+    flag: "🇻🇳",
+    lat: 20.2506,
+    lng: 105.9745,
+    images: [],
+    date: "",
+    events: [
+      {
+        id: 1,
+        name: "Visit Cuc Phuong Forest and Hoa Lu Ancient Capital with my beloved friend",
+        date: "May 2025",
+        images: [HoaLu1, HoaLu2, HoaLu3, HoaLu4, HoaLu5, HoaLu6, HoaLu7, HoaLu8],
+      },
+    ],
+  },
+  {
+    id: 4,
+    city: "Ninh Thuan",
+    country: "Vietnam",
+    flag: "🇻🇳",
+    lat: 11.6739,
+    lng: 109.1500,
+    images: [],
+    date: "",
+    events: [
+      {
+        id: 1,
+        name: "April 2025 trip to Vinh Hy Bay with my beloved friend, enjoying the beautiful beach, delicious seafood, and stunning landscapes. First time with lobster.",
+        date: "",
+        images: [
+          VinhHy1,  VinhHy2,  VinhHy3,  VinhHy4,  VinhHy5,
+          VinhHy6,  VinhHy7,  VinhHy8,  VinhHy9,  VinhHy10,
+          VinhHy11, VinhHy12, VinhHy13, VinhHy14, VinhHy15,
+          VinhHy16, VinhHy17, VinhHy18, VinhHy19, VinhHy20,
+          VinhHy21, VinhHy22, VinhHy23, VinhHy24, VinhHy25,
+          VinhHy26, VinhHy28, VinhHy29,
+        ],
       },
     ],
   },
